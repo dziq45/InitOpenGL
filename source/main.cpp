2a56123611fd4934825edcc1ca4c6f8dfb7ab4f1
dziq45 <piotr.papriko@gmail.com>
2021-05-25 00:50:06 +0200

    uniform us

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <stdio.h>
+#include <math.h>
 
 namespace {
    void errorCallback(int error, const char* description) {
@@ -50,24 +51,24 @@ int main(int argc, char* argv[]) {
    }
    const char *vertexShaderSource = "#version 330 core\n"
       "layout (location = 0) in vec3 aPos;\n"
+      "layout (location = 1) in vec3 aColor;\n"
+      "out vec3 ourColor;\n"
       "void main()\n"
       "{\n"
       "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+      "   ourColor = aColor;\n"
       "}\0";
    const char *fragmentShaderSource = "#version 330 core\n"
       "out vec4 FragColor;"
+      "uniform vec4 ourColor;\n"
       "void main()\n"
       "{\n"
-      "FragColor = vec4(1.0f, 0.3f, 0.7f, 1.0f);\n"
+      "FragColor = ourColor;\n"
       "}\0"; 
    float vertices[] = {
-     0.0f, 0.5f, 0.0f,
-     0.0f, 0.0f, 0.0f,
-     0.5f, 0.0f, 0.0f,
-
-     0.0f, 0.0f, 0.0f,
-     0.0f,-0.5f, 0.0f,
-     -0.5f,0.0f, 0.0f
+     0.0f, 0.5f, 0.0f,1.0f, 0.0f, 0.0f,
+     0.0f, 0.0f, 0.0f,0.0f, 1.0f, 0.0f,
+     0.5f, 0.0f, 0.0f,0.0f, 0.0f, 1.0f
    };
 
    //creating vertex buffer object
@@ -123,12 +124,14 @@ int main(int argc, char* argv[]) {
    glBindBuffer(GL_ARRAY_BUFFER, VBO);
    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
    //we have to specyify how OPENGL should interpret vertex 
-   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
    glEnableVertexAttribArray(0);
 
    //unbind VBO and VAO
    glBindBuffer(GL_ARRAY_BUFFER, 0); 
    glBindVertexArray(0);
+
+   int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
    
 
    // Set the clear color to a nice green
@@ -139,8 +142,11 @@ int main(int argc, char* argv[]) {
       glClear(GL_COLOR_BUFFER_BIT);
 
       glUseProgram(shaderProgram);
+      float timeValue = glfwGetTime();
+      float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+      glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
        // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-      glDrawArrays(GL_TRIANGLES, 0, 6);
+      glDrawArrays(GL_TRIANGLES, 0, 3);
       
       glfwSwapBuffers(window);
       glfwPollEvents();
